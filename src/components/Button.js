73c3b35dafefa83c2a70ce8b5e3e2e8ce3f448b2
HankBedford <Zachary.Lynch1337@gmail.com>
2018-12-15 11:15:22 -0800

    added all necessary buttons and styled the buttons to look like apple calculator

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Button.css';
 
+// checks if the number passed to val is a number or not
+const isOperator = val => {
+    return !isNaN(val) || val === "." || val === "=";
+}
+
 
 const Button = (props) => {
     return(
-    <div>
+    <div className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`}>
         {props.children}
     </div>
     )
